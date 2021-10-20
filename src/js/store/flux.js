ab0fa9166af6f73e2e883d630ca73173e36531f0
andresgoag <andresgomezagudelo@hotmail.com>
2021-10-20 19:06:39 +0000

    created getcontacts

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,28 @@
+const URL_API = "https://assets.breatheco.de/apis/fake/contact";
+const agenda_slug = "andresgoag";
+
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			contacts: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+
+			getContacts: () => {
+				fetch(`${URL_API}/agenda/${agenda_slug}`)
+					.then(response => {
+						if (response.ok) {
+							return response.json();
+						} else {
+							return new Error("Error fetching the api");
+						}
+					})
+					.then(data => setStore(data))
+					.catch(error => console.error(error));
+			}
 		}
 	};
 };
